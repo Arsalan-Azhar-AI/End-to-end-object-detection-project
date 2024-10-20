9ae5150c1021d4a7e7876376ab1ad75e298622f7
Arsalan Khan <arsalanazhar2003@gmail.com>
2024-10-20 05:16:40 +0500

    Data Ingestion done through github url

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from signLanguage.logger import logging
 from signLanguage.exception import SignException
 import sys
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 
+obj=TrainPipeline()
+obj.run_pipeline()
